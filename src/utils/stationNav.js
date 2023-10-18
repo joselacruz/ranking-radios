f35f89028a1091c2bbd8e78e24404cf09b479913
joselacruz <joselacruzperez@gmail.com>
2023-10-18 02:38:44 -0400

    Optimization: Avoid rendering again if we are already in the details view of a station.
    This commit optimizes the 'navigateWithStationDetails' function to avoid unnecessary updates. Now, before sending the details of a station to the context and navigating to the new route, it is checked if we are already in the details view of that station. If so, status updating and navigation are avoided, thus reducing redundant renderings and duplication of information in the station history.

--- src/utils/stationNav.js
+++ src/utils/stationNav.js
@@ -9,18 +9,30 @@ const useStationNavigation = () => {
   const context = useContext(StationContext)
 
   const navigateWithStationDetails = ( {station}) => {
+      // Crea una ruta amigable para la estación eliminando espacios y usando guiones
+      const createUrl = station.name.split(" ").join("-");
+      const routePathname = `/view/${createUrl}`;
 
-    // Establece los detalles de la estación en el contexto.
-    context.setStationDetails(station);
-
-    // Envia La Estacion al Historial de Estaciones del Contexto
-    context.addHistoryStatios(station);
-
-    // Crea una ruta amigable para la estación eliminando espacios y usando guiones
-    const createRoute = station.name.split(" ").join("-");
-
-    // Navega a la ruta de detalles de la estación.
-    location(`/view/${createRoute}`);
+/**
+ * Evita actualizaciones innecesarias si ya estamos en la vista de detalles de la estación actual.
+ * Comprueba si la ubicación actual de la ventana no coincide con la ruta de detalles de la estación.
+ * Esto previene la actualización del estado del contexto destinado a la vista de detalles de la estación.
+ * En resumen, si window.location.pathname es igual a routePathname,
+ * significa que ya estamos viendo los detalles de esa estación y no es necesario actualizar el estado.
+ */
+    if(window.location.pathname !== routePathname ){
+  
+          // Establece los detalles de la estación en el contexto.
+      context.setStationDetails(station);
+  
+      // Envia La Estacion al Historial de Estaciones del Contexto
+      context.addHistoryStatios(station);
+  
+      // Navega a la ruta de detalles de la estación.
+      location(routePathname);
+      
+    }
+  
   };
 
   return navigateWithStationDetails;
