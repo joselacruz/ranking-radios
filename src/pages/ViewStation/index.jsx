cc93013834f8c258e71732c0bbe5d9ffcf8dae4a
joselacruz <joselacruzperez@gmail.com>
2023-10-18 01:36:17 -0400

    Component RadioStationInfoCard: Add load handling and error messages to get real votes from the radio station
    Component serverRequestUtil: settings in server request utility to show all available tags of a station without clipping them

--- src/pages/ViewStation/index.jsx
+++ src/pages/ViewStation/index.jsx
@@ -1,26 +1,67 @@
 import { Layout } from "../../components/Layout";
-import { Box, Container, Typography, IconButton, Stack } from "@mui/material";
+import { Box, Container, Typography, Stack } from "@mui/material";
 import { StationContext } from "../../context/StationContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PlayPauseIcon } from "../../components/PlayPauseIcon";
 import { FavoriteButton } from "../../components/FavoriteButton";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import { RadioStationInfoCard } from "../../components/RadioStationInfoCard";
 import { VoteIcon } from "../../components/VoteIcon";
+import { makeServerRequest } from "../../utils/serverRequestUtil";
 
 const ViewStation = () => {
   const context = useContext(StationContext);
+  // Estado local para manejar la carga de votos
+  const [loadingVotes, setLoadingVotes] = useState(true);
 
+  // Objeto con información de la estación para mostrar en la interfaz de usuario
   const stationInfo = {
     location: context.stationDetails.country || "undefined",
     language: context.stationDetails.language || "undefined",
     website: context.stationDetails.homepage || "undefined",
     genders: context.stationDetails.tags || "undefined",
-    votes: context.stationDetails.votes.toLocaleString() || "undefined",
+    votes: loadingVotes
+      ? "charging..." // Muestra este mensaje mientras se obtienen los votos reales
+      : context.stationDetails.votes.toLocaleString() || "undefined",
     codec: context.stationDetails.codec || "undefined",
     bitrate: context.stationDetails.bitrate || "undefined",
   };
 
+  // Función asincrónica para obtener los votos reales desde la API
+  async function fetchActualVotes() {
+    try {
+      // Identificador único de la estación
+      const identifier = context.stationDetails.stationuuid;
+
+      // Realiza la solicitud a la API para obtener la información más reciente de la estación
+      const response = await makeServerRequest({
+        endpoint: `json/stations/byuuid/${identifier}`,
+        limit: 1,
+      });
+
+      // Obtiene el número actual de votos desde la respuesta de la API
+      const currentVoteServer = response[0].votes;
+
+      // Compara los votos actuales con los almacenados en el contexto y actualiza si es necesario
+      if (currentVoteServer !== context.stationDetails.votes) {
+        context.setStationDetails((prevState) => ({
+          ...prevState,
+          votes: currentVoteServer,
+        }));
+      }
+    } catch (error) {
+      // Maneja errores durante la solicitud e imprime detalles en la consola
+      console.error("Error fetching actual votes:", error);
+    } finally {
+      setLoadingVotes(false); // Cuando la solicitud termina, establece loading en false
+    }
+  }
+
+  // Efecto secundario que se ejecuta después del montaje del componente
+  useEffect(() => {
+    fetchActualVotes(); // Inicia la obtención de votos al montar el componente
+  }, []); // La dependencia vacía indica que solo se ejecuta una vez al montar el componente
+
+  // Renderiza la interfaz de usuario con la información de la estación y botones de interacción
   return (
     <Layout>
       <Container maxWidth={"md"}>
--- src/utils/serverRequestUtil.js
+++ src/utils/serverRequestUtil.js
@@ -1,9 +1,5 @@
 // serverRequestUtil.js
 
-import axios from "axios";
-
-
-
 // Función para obtener la lista de servidores
 // export async function getServerList() {
 //     try {
@@ -18,75 +14,80 @@ import axios from "axios";
 //       return [{url:'http://nl1.api.radio-browser.info/'}];
 //     }
 //   }
-  
-  // Función para realizar solicitudes a la API con cada dirección de servidor
 
+// Función para realizar solicitudes a la API con cada dirección de servidor
 
+const serverList = [
+  { url: "https://nl1.api.radio-browser.info/" },
+  { url: "https://de1.api.radio-browser.info/" },
+  { url: "https://fr1.api.radio-browser.info/" },
+  { url: "https://at1.api.radio-browser.info/" },
+];
+export async function makeServerRequest({
+  endpoint,
+  offset = 0,
+  limit,
+  otherQuery = null,
+}) {
+  const timeoutMs = 2000; // 10 segundos (ajusta este valor según tus necesidades)
 
-
-
-
-
-const serverList = [{
-  url:'https://nl1.api.radio-browser.info/'},
-{url:'https://de1.api.radio-browser.info/'},
-{url:'https://fr1.api.radio-browser.info/'},
-{url:'https://at1.api.radio-browser.info/'}
-]
-export async function makeServerRequest({ endpoint, offset=0,limit,otherQuery=null }) {
-const timeoutMs = 2000; // 10 segundos (ajusta este valor según tus necesidades)
-
-
-const queryParams = new URLSearchParams();
-if (otherQuery) {
-  for (const key in otherQuery) {
-    if (otherQuery.hasOwnProperty(key)) {
-      queryParams.append(key, otherQuery[key]);
+  const queryParams = new URLSearchParams();
+  if (otherQuery) {
+    for (const key in otherQuery) {
+      if (otherQuery.hasOwnProperty(key)) {
+        queryParams.append(key, otherQuery[key]);
+      }
     }
   }
-}
-const queryString = queryParams.toString();
+  const queryString = queryParams.toString();
 
+  for (const server of serverList) {
+    const serverUrl = server.url;
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => {
+        controller.abort(); // Abortar la solicitud si supera el límite de tiempo
+      }, timeoutMs);
+      const laurl = `${serverUrl}${endpoint}?offset=${offset}&limit=${limit}${
+        queryString ? `&${queryString}` : ""
+      }`;
 
+      const response = await fetch(
+        `${serverUrl}${endpoint}?offset=${offset}&limit=${limit}${
+          queryString ? `&${queryString}` : ""
+        }`,
+        {
+          signal: controller.signal,
+        }
+      );
+      clearTimeout(timeoutId); // Limpiar el temporizador si la solicitud se completa a tiempo
 
-for (const server of serverList) {
-  const serverUrl = server.url;
-  try {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => {
-      controller.abort(); // Abortar la solicitud si supera el límite de tiempo
-    }, timeoutMs);
-   const laurl = `${serverUrl}${endpoint}?offset=${offset}&limit=${limit}${queryString ? `&${queryString}` : ''}`;
-   
-   const response = await fetch(`${serverUrl}${endpoint}?offset=${offset}&limit=${limit}${queryString ? `&${queryString}` : ''}`, {
-    signal: controller.signal,
-  });
-    clearTimeout(timeoutId); // Limpiar el temporizador si la solicitud se completa a tiempo
+      if (response.ok) {
+        const data = await response.json();
+        const parsed = data.map((item) => {
+          const tagsArray = item.tags?.split(",").map((tag) => tag.trim());
+          return {
+            ...item,
+            tags: tagsArray,
+          };
+        });
 
-    if (response.ok) {
-      const data = await response.json();
-      const parsed = data.map((item) => {
-        const tagsArray = item.tags?.split(',').map((tag) => tag.trim());
-        return {
-          ...item,
-          tags: tagsArray?.length > 3 ? tagsArray.slice(0, 3) : tagsArray,
-        };
-      });
-
-      return parsed; // Retorna los datos si la solicitud fue exitosa
-    } else {
-      console.error(`La solicitud a ${serverUrl} no fue exitosa: ${response.status}`);
+        return parsed;
+      } else {
+        console.error(
+          `La solicitud a ${serverUrl} no fue exitosa: ${response.status}`
+        );
+      }
+    } catch (error) {
+      console.error(
+        `Error al hacer la solicitud a ${serverUrl}: ${error.message}`
+      );
     }
-  } catch (error) {
-    console.error(`Error al hacer la solicitud a ${serverUrl}: ${error.message}`);
   }
+
+  // Si ninguna solicitud fue exitosa, puedes manejarlo aquí
+  return;
 }
-
-// Si ninguna solicitud fue exitosa, puedes manejarlo aquí
-return ;
-}
-
-
 
 // const serverList = [{
 //   url:'https://nl1.api.radio-browser.info/'},
